0876b991ee0755d63f012ff8614a428794abbe73
Allan G Rizza <allanrizza.dev@gmail.com>
2023-02-22 23:54:36 -0300

    Using new features

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,16 +1,19 @@
 import algorithms.BubbleSort;
 import datastructures.Array;
+import utils.DisplayUtils;
 
 public class Application {
     public static void main(String[] args) {
-        System.out.println("================================= BUBBLE SORT ======================================");
         Array array = new Array(new int[]{5, 2, 4, 6, 1, 3});
+        BubbleSort bubbleSort = new BubbleSort();
+        DisplayUtils.displayStartAlgorithm(bubbleSort);
         System.out.println("ORIGINAL ARRAY:");
         array.display();
 
         BubbleSort.sort(array);
-        System.out.println("SORTED ARRAY AFTER BUBBLE SORT ALGORITHM (algorithm with complexity O(N²)):");
+
+        System.out.println("SORTED ARRAY AFTER BUBBLE SORT ALGORITHM (algorithm with complexity "+ bubbleSort.getComplexity() +"):");
         array.display();
-        System.out.println("====================================================================================");
+        DisplayUtils.displayEndAlgorithm(bubbleSort);
     }
 }
