cb65ab18dc2ba7b924248427c01226df08785900
Allan G Rizza <allanrizza.dev@gmail.com>
2023-02-22 23:53:55 -0300

    Extending Algorithm in BubbleSort

--- src/main/java/algorithms/BubbleSort.java
+++ src/main/java/algorithms/BubbleSort.java
@@ -1,8 +1,15 @@
 package algorithms;
 
+import algorithms.enums.ComplexityEnum;
 import datastructures.Array;
 
-public class BubbleSort {
+public class BubbleSort extends Algorithm {
+
+    public BubbleSort() {
+        this.setName("BUBBLE SORT");
+        this.setComplexity(ComplexityEnum.ON2);
+    }
+
     public static void sort(Array array) {
         for (int i = array.values.length-1; i > 0; i--) {
             for(int j = 0; j < i; j++) {
