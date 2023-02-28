e810f9b111a4c2814dc854bb072d5fb36d4442e6
Allan G Rizza <allanrizza.dev@gmail.com>
2023-02-28 19:37:59 -0300

    Creating LaforeAlgorithm's conditional display

--- src/main/java/utils/DisplayUtils.java
+++ src/main/java/utils/DisplayUtils.java
@@ -1,6 +1,7 @@
 package utils;
 
 import algorithms.Algorithm;
+import algorithms.LaforeAlgorithm;
 
 /**
  * A very simple utils class to display a nice visual presentation for an algorithm.
@@ -8,10 +9,25 @@ import algorithms.Algorithm;
  */
 public class DisplayUtils {
     public static void displayStartAlgorithm(Algorithm algorithm) {
-        System.out.println("================================= START " + algorithm.getName() + " ======================================");
+        if(algorithm instanceof LaforeAlgorithm)
+        {
+            System.out.println("================================= START LAFORE'S ALGORITHM " + algorithm.getName() + " ======================================");
+        }
+        else
+        {
+            System.out.println("================================= START " + algorithm.getName() + " ======================================");
+        }
+
     }
 
     public static void displayEndAlgorithm(Algorithm algorithm) {
-        System.out.println("================================== END " + algorithm.getName() + " =======================================\n\n\n");
+        if(algorithm instanceof LaforeAlgorithm)
+        {
+            System.out.println("================================== END LAFORE'S ALGORITHM " + algorithm.getName() + " =======================================\n\n\n");
+        }
+        else
+        {
+            System.out.println("================================== END " + algorithm.getName() + " =======================================\n\n\n");
+        }
     }
 }
