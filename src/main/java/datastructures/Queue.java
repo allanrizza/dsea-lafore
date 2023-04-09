ad6806f8b310ab0f7dd686175de432df097ebfac
Allan Rizza <allanrizza.dev@gmail.com>
2023-04-09 12:20:04 -0300

    queue data structure

---
+++ src/main/java/datastructures/Queue.java
@@ -0,0 +1,78 @@
+package datastructures;
+
+
+/**
+ * @author Allan G. Rizza
+ */
+public class Queue {
+    private int maxSize;
+    private long[] values;
+    private int front;
+    private int rear;
+    private  int numItems;
+
+    public Queue(int num) {
+        this.maxSize = num;
+        this.values = new long[maxSize];
+        this.front = 0;
+        this.rear = -1;
+        this.numItems = 0;
+    }
+
+    public void insert(int num) {
+        if(rear == maxSize-1) {
+            rear = -1;
+        }
+        this.values[++rear] = num;
+        numItems++;
+    }
+
+    public long remove() {
+        long temp = values[front++];
+        if(front == maxSize) {
+            front = 0;
+        }
+        numItems--;
+        return temp;
+    }
+
+    public long peek() {
+        return values[front];
+    }
+
+    public boolean isEmpty() {
+        return this.numItems == 0;
+    }
+
+    public boolean isFull() {
+        return this.numItems == maxSize;
+    }
+
+    public int size() {
+       return this.numItems;
+    }
+
+    public static void example() {
+        Queue queue = new Queue(5);
+        queue.insert(10);
+        queue.insert(20);
+        queue.insert(30);
+        queue.insert(40);
+
+        queue.remove();
+        queue.remove();
+        queue.remove();
+
+        queue.insert(50);
+        queue.insert(60);
+        queue.insert(70);
+        queue.insert(80);
+
+        while(!queue.isEmpty()) {
+            long n = queue.remove();
+            System.out.println(n);
+            System.out.println("  ");
+        }
+        System.out.println("  ");
+    }
+}
